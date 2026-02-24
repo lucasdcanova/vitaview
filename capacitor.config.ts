dd5ad0d653e771dfa90ec07de63184eee466727a
Lucas Canova <lucas.canova@icloud.com>
2026-02-24 18:43:57 -0300

    setup ios capacitor project and xcode cloud pre-build script

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'br.com.lucascanova.vitaview',
+  appName: 'VitaView',
+  webDir: 'dist/public'
+};
+
+export default config;
